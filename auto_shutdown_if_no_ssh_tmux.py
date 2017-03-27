605e5457f38b353974f3da1f70a615101d81a4e0
plainroc <plainroc@hotmail.com>
2017-03-28 00:10:47 +0800

    check started time and really shutdown.

--- auto_shutdown_if_no_ssh_tmux.py
+++ auto_shutdown_if_no_ssh_tmux.py
@@ -17,6 +17,15 @@ def has_GPU_task():
     status, output = commands.getstatusoutput("nvidia-smi --query-compute-apps=name --format=csv,noheader | wc -l")
     return status == 0 and string.atoi(output) > 0
 
+def has_started_for(seconds):
+    status, output = commands.getstatusoutput("awk -F. '{print $1}' /proc/uptime")
+    if status ==0 and string.atoi(output) > seconds:
+        return True
+    else if status != 0:
+        return True
+    else:
+        return False
+
 def main():
     have_ssh_connect = ssh_connection_count() > 0
     if have_ssh_connect:
@@ -29,8 +38,11 @@ def main():
     if has_GPU_task():
         print("there are GPU tasks.")
         return
+    if not has_started_for(60*10): # if only start less than 10 minutes, maybe we not ssh yet.
+        print("jsut start, wait for ssh.")
+        return
     print("should shutdown now.")
-    #os.system("sudo shutdown -P now")
+    os.system("sudo shutdown -P now")
     
 if __name__ == '__main__':
     main()
