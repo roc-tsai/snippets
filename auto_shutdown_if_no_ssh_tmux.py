52d2271d438b98b71c0e6b815e3dc70e1d5dd9fb
plainroc <plainroc@hotmail.com>
2017-04-06 00:11:12 +0800

    fix compile error.

--- auto_shutdown_if_no_ssh_tmux.py
+++ auto_shutdown_if_no_ssh_tmux.py
@@ -30,22 +30,25 @@ def has_started_for(seconds):
 def main():
     have_ssh_connect = ssh_connection_count() > 0
     logfile = "/home/titan/log/auto_shutdown_check.log"
-    os.system("echo check auto shutdown at " + datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + " > " + logfile)
-    if have_ssh_connect:
-        os.system("echo there are ssh connection. >> " + logfile)
-        return
-    have_tmux_session = exist_tmux_session()
-    if have_tmux_session:
-        os.system("echo there are tmux sessions. >> " + logfile)
-        return
-    if has_GPU_task():
-        os.system("echo there are GPU tasks. >> " + logfile)
-        return
-    if not has_started_for(60*10): # if only start less than 10 minutes, maybe we not ssh yet.
-        os.system("echo jsut start, wait for ssh. >> " + logfile)
-        return
-    os.system("echo should shutdown now. >> " + logfile)
-    os.system("sudo shutdown -P now")
+    os.system("echo check auto shutdown at " + datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + " >> " + logfile)
+    try:
+        if have_ssh_connect:
+            os.system("echo there are ssh connection. >> " + logfile)
+            return
+        have_tmux_session = exist_tmux_session()
+        if have_tmux_session:
+            os.system("echo there are tmux sessions. >> " + logfile)
+            return
+        if has_GPU_task():
+            os.system("echo there are GPU tasks. >> " + logfile)
+            return
+        if not has_started_for(6*60): # if only start less than 6 minutes, maybe we not ssh yet.
+            os.system("echo jsut start, wait for ssh. >> " + logfile)
+            return
+        os.system("echo should shutdown now. >> " + logfile)
+        os.system("sudo shutdown -P now")
+    finally:
+        os.system("echo check finish. >> " + logfile)
     
 if __name__ == '__main__':
     main()
